bb8e57d6d8dfdc972da10442a5d79a8bb6200dc4
jbialon32 <jbialon32@gmail.com>
2021-05-29 16:55:07 -0500

    added a method for creating multiple objects and documentation on the object maker.

--- src/main/java/com/revature/querinator/GenericObjectMaker.java
+++ src/main/java/com/revature/querinator/GenericObjectMaker.java
@@ -8,15 +8,27 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GenericObjectMaker {
 
     public GenericObjectMaker() {
 
     }
+
+    /**
+     *
+     * Description: Builds and returns a single object.
+     *
+     * @param clazz
+     * @param objInfo
+     * @return
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws SQLException
+     */
 
     public Object buildObject(Class clazz, List<Map<String, Object>> objInfo) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, SQLException {
 
@@ -68,6 +80,77 @@ public class GenericObjectMaker {
         }
 
         return newObj;
+    }
+
+    /**
+     *
+     * Description: Builds and returns multiple objects in a Deque.
+     *
+     * @param clazz
+     * @param objInfo
+     * @return
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws SQLException
+     */
+    public Deque<Object> buildObjects(Class clazz, List<Map<String, Object>> objInfo) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, SQLException {
+
+        Deque<Object> returnVal = new ArrayDeque<>();
+
+        Object newObj = clazz.getDeclaredConstructor().newInstance();
+
+        // POJO Class's fields
+        Field[] classFields = clazz.getDeclaredFields();
+
+        // Field level annotations
+        Annotation[] fieldAnno;
+
+        int indexCount = 0;
+
+        while (indexCount < objInfo.size()) {
+
+            newObj = clazz.getDeclaredConstructor().newInstance();
+
+            Map currObjInfo = objInfo.get(indexCount);
+
+            // Start looping through the class fields
+            for (Field field : classFields) {
+
+                // Grab the current fields annotations
+                fieldAnno = field.getAnnotations();
+
+                // Loop through the annotations in the previous step
+                for (Annotation ano : fieldAnno) {
+
+                    if (ano instanceof Primary) {
+
+                        field.setAccessible(true);
+                        field.set(newObj, currObjInfo.get(field.getAnnotation(Primary.class).name()));
+                        field.setAccessible(false);
+
+                    }
+
+                    if (ano instanceof Column) {
+
+                        field.setAccessible(true);
+                        field.set(newObj, currObjInfo.get(field.getAnnotation(Column.class).name()));
+                        field.setAccessible(false);
+
+                    }
+
+                }
+
+            }
+
+            returnVal.add(newObj);
+
+            indexCount++;
+
+        }
+
+        return returnVal;
     }
 
 }
